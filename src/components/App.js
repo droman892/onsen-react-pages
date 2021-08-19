64a4b06b0767bcc3a42d076a82549b3b646cd914
droman892 <droman892@gmail.com>
2021-08-18 21:24:06 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <Page1/>
+        )
+    }
+
+
+
+}
+
+
+
+export default App;
---
+++ src/components/Page1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputText from './InputText';
+import InputDate from './InputDate';
+import Checkbox from './Checkbox';
+import RadioList from './RadioList';
+import Select from './Select';
+import ButtonNavigator from './ButtonNavigator';
+
+
+
+
+
+export default Page1;
